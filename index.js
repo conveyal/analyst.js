1f9e6453618d8e23c92e3f9e6ae43bcfc5480213
Trevor Gerhardt <trevorgerhardt@gmail.com>
2015-06-16 15:50:27 +0200

    Remove fetch usage from shapefiles function and fix url

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ export default class Analyst {
 
   shapefiles () {
     debug('fetching available shapefiles')
-    return window.fetch(this.apiUrl + '/shapefiles').then(r => r.json())
+    return get(this.apiUrl + '/shapefile').then(r => r.json())
   }
 
   /**
@@ -183,5 +183,20 @@ function post (url, data) {
     req.on('error', reject)
     req.write(JSON.stringify(data))
     req.end()
+  })
+}
+
+function get (url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, (res) => {
+      let data = ''
+      res.on('error', reject)
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+      res.on('close', () => {
+        resolve(JSON.parse(data))
+      })
+    }).on('error', reject)
   })
 }
